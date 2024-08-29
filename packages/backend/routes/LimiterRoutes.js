a166b9e9e42e4d629170bd1d81e9d667ff2424d3
Daniel Yates <145247690+Acid-base@users.noreply.github.com>
2024-08-28 23:50:40 -0600

    restructure
    went on a side quest to learn a bit more python so I could suplex these annoying CSV SQL dumps into coherent data, learned more Mongodb best practices by testing some transformations, compound indexes, denormalization where they made sense to use.

--- packages/backend/routes/LimiterRoutes.js
+++
@@ -1,9 +0,0 @@
-import rateLimit from 'express-rate-limit';
-
-const apiLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 15, // Limit each IP to 15 requests per minute
-  message: { error: 'Too many requests, please try again later.' },
-});
-
-export default apiLimiter;
